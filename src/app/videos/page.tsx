240a4d85f963f4c75326905b924fcd25d3c60a5b
alhiefikri <alhiepedia@gmail.com>
2025-02-26 00:18:17 +0800

    feat: styling component

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,3 +1,21 @@
-export default function Videos() {
-  return <h2>Video Page</h2>;
+import { SearchInput } from "@/components/SearchInput";
+import VideoList from "@/components/videos/VideoList";
+
+export default function Videos({
+  searchParams,
+}: {
+  searchParams: {
+    [key: string]: string;
+  };
+}) {
+  return (
+    <div>
+      <div className="w-[50%]">
+        <SearchInput />
+      </div>
+      <div className="mt-8">
+        <VideoList />
+      </div>
+    </div>
+  );
 }
