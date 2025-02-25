3c4af0f710faa1fc6acd7b664b40aaa2a4c37614
alhiefikri <alhiepedia@gmail.com>
2025-02-25 23:53:55 +0800

    feat: adding shadcn component, aplly darkmode toggle

---
+++ src/app/videos/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function VideoDetail({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  return <h2>Video Detail {params.id}</h2>;
+}
---
+++ src/app/videos/page.tsx
@@ -0,0 +1,3 @@
+export default function Videos() {
+  return <h2>Video Page</h2>;
+}
