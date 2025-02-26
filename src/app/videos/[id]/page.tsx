81e51180e28e03479a0e9f556f05a2224700c178
alhiefikri <alhiepedia@gmail.com>
2025-02-26 21:05:22 +0800

    feat: Video item section

--- src/app/videos/[id]/page.tsx
+++ src/app/videos/[id]/page.tsx
@@ -1,20 +1,44 @@
-export default function VideoDetail({
+import { apiKey } from "@/constant";
+
+async function fetchVideo(id: string) {
+  let url = new URL(
+    `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${apiKey}`
+  );
+
+  url.searchParams.set("part", "snippet");
+  url.searchParams.set("type", "video");
+
+  try {
+    const response = await fetch(url);
+    const video = await response.json();
+    return video.items;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function VideoDetail({
   params,
 }: {
   params: {
     id: string;
   };
 }) {
+  const videoSrc = `https://www.youtube.com/embed/${params.id}`;
+  const video = await fetchVideo(params.id);
+  console.log(video);
   return (
     <div className="px-8 grid grid-cols-10 gap-4">
       <div className="col-span-7">
         <div className="w-[100%]">
-          <iframe src="" className="w-[100%] h-[35rem]" />
+          <iframe src={videoSrc} className="w-[100%] h-[35rem]" />
         </div>
         <div className="flex flex-col gap-1">
-          <h2 className="font-bold text-lg">Title</h2>
-          <h2 className="font-bold text-sm">Channel Title</h2>
-          <p className="text-slate-400 text-md">Desctiption</p>
+          <h2 className="font-bold text-lg">{video[0].snippet.title}</h2>
+          <h2 className="font-bold text-sm">{video[0].snippet.channelTitle}</h2>
+          <p className="text-slate-400 text-md">
+            {video[0].snippet.description}
+          </p>
         </div>
       </div>
     </div>
